ef4b3bebbb584ab7a4d5b1edeb6b17828954a4aa
Nathan Teissier <nmteissier@gmail.com>
2023-01-12 14:27:13 +0100

    final commit android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,8 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'BankOne_v2',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  hideLogs: false
 };
 
 export default config;
